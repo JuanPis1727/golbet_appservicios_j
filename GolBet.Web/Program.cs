eab2b712af691c0327899fa4d52433b6bfa45d1e
juanpablo6+ <juanpablozuluagasalazar6@gmail.com>
2026-09-03 16:14:10 -0500

    Add generic repository, match repository and database seeder (Module 3)

--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,19 +1,36 @@
 using GolBet.Repositories.Data;
+using GolBet.Repositories.Implementations;
+using GolBet.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Registro de Servicios (Controllers, DbContext, Inyección de Dependencias)
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositorio genérico abierto
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+// Repositorios específicos
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+
+// 2. Construcción de la aplicación
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 3. Ejecución del Seeder al arrancar (Scope temporal)
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await DbSeeder.SeedAsync(context);
+}
+
+// 4. Configuración del pipeline HTTP (Middleware)
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
